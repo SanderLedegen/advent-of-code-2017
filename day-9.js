af776e8ac41616f456c37dbdb943d129e6698caa
Sander Ledegen <sander.ledegen@outlook.com>
2017-12-11 16:09:25 +0100

    Day 9 finished! 😊

---
+++ day-9.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+const input = fs.readFileSync('./input-day-9.txt', 'utf-8')
+
+const result = solvePartOneAndTwo(input)
+
+// Answers to the challenge 🤓
+console.log(result.sum) // 14212
+console.log(result.numOfGarbageCharacters) // 6569
+
+function solvePartOneAndTwo(input) {
+  const chars = [...input]
+  let nestedLevel = 0
+  let isGarbage = false
+  let ignoreNextCharacter = false
+  let numOfGarbageCharacters = 0
+
+  const sum = chars.reduce((sum, char) => {
+    if (ignoreNextCharacter) {
+      ignoreNextCharacter = false
+      return sum
+    }
+
+    switch (char) {
+      case '{':
+        if (!isGarbage) {
+          nestedLevel += 1
+        } else {
+          numOfGarbageCharacters += 1
+        }
+        break
+      case '}':
+        if (!isGarbage) {
+          sum += nestedLevel
+          nestedLevel -= 1
+        } else {
+          numOfGarbageCharacters += 1
+        }
+        break
+      case '<':
+        // If the current characters in the stream were already marked as garbage, this char is garbage as well.
+        if (isGarbage) {
+          numOfGarbageCharacters += 1
+        }
+        isGarbage = true
+        break
+      case '>':
+        isGarbage = false
+        break
+      case '!':
+        ignoreNextCharacter = true
+        break
+      default:
+        if (isGarbage) {
+          numOfGarbageCharacters += 1
+        }
+        break
+    }
+
+    return sum
+  }, 0)
+
+  return { sum, numOfGarbageCharacters }
+}
