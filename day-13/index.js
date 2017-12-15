dcf4a92cfa57086aa9a391fe1f3d8759df1497f6
Sander Ledegen <sander.ledegen@outlook.com>
2017-12-15 17:06:49 +0100

    Day 13 😌

--- day-13/index.js
+++ day-13/index.js
@@ -3,13 +3,70 @@ const path = require('path')
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf-8')
 
 // Answers to the challenge 🤓
-console.log(solvePartOne(input)) // 
-console.log(solvePartTwo(input)) // 
+const scanners = parseInput(input)
+console.log(solvePartOne(scanners)) // 1632
+console.log(solvePartTwo(scanners)) // 3834136
 
-function solvePartOne(input) {
-  //
+function solvePartOne(scanners) {
+  const maxSteps = Math.max(...Object.keys(scanners))
+  let step = 0
+  let severity = 0
+
+  while (step <= maxSteps) {
+    const range = scanners[step]
+
+    if (range) {
+      // I quote, "If there is a scanner at the top of the layer as your packet enters it, you are caught.""
+      const caught = step % (2 * (range - 1)) === 0
+      if (caught) {
+        severity += step * range
+      }
+    }
+
+    step += 1
+  }
+
+  return severity
 }
 
-function solvePartTwo(input) {
-  //
+/**
+ * Note, I'm not proud of my approach (looping and all) but I got my result in the end so... ¯\_(ツ)_/¯
+ */
+function solvePartTwo(scanners) {
+  const maxSteps = Math.max(...Object.keys(scanners))
+  let delay = 0
+
+  // Fuck it. Run, Forest, run! Run until you find a solution! 😆
+  while (true) {
+    let step = 0
+    let wrongDelay = false
+
+    while (step <= maxSteps) {
+      const range = scanners[step]
+  
+      if (range) {
+        const caught = (step + delay) % (2 * (range - 1)) === 0
+        if (caught) {
+          wrongDelay = true
+          break
+        }
+      }
+  
+      step += 1
+    }
+
+    if (!wrongDelay) {
+      return delay
+    }
+
+    delay += 1
+  }
+}
+
+function parseInput(input) {
+  return input.split('\n').reduce((map, line) => {
+    const parts = line.split(': ').map(i => +i)
+    map[parts[0]] = parts[1]
+    return map
+  }, {})
 }
